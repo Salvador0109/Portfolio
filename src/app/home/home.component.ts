6ac2eaed817660b426fd6e15aa577366558af4a0
Salvador0109 <salvador_0109@hotmail.com>
2024-01-15 02:59:21 -0600

    Estructura de banner

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from './banner/banner.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BannerComponent],
+  imports: [BannerComponent, AboutComponent, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
